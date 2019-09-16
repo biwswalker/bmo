436b540db88e70322f4c22cb965a9be50c9048c6
Biws <jennarong.sae@gmail.com>
2019-09-16 23:36:57 +0700

    filter highlight api

--- src/api/heightlight.js
+++ src/api/heightlight.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import moment from 'moment'
+import _ from 'lodash'
 import { times } from '@utils'
 import { highlightService } from '@services'
 import { RESPONSE_CODE, RESPONSE_STATUS } from '@constants'
@@ -9,11 +10,13 @@ const HIGHLIGHT_ENDPOINT = '/highlight'
 const router = express.Router()
 
 router.get(HIGHLIGHT_ENDPOINT, (req, res) => {
+  console.log(`${HIGHLIGHT_ENDPOINT}...`)
   const currentMonth = Number(moment().format('M'))
   const season = times.season(currentMonth)
   highlightService.getHighlightPlant(season.name, (response, error) => {
     if (error) { return res.json({ code: RESPONSE_CODE.INTERNAL_SERVER_ERROR, status: RESPONSE_STATUS.FAILURE, error }) }
-    return res.json({ code: RESPONSE_CODE.OK, status: RESPONSE_STATUS.SUCCESS, data: response })
+    const highlight = response.map(highlightResponse => _.pick(highlightResponse, ['_id', 'name', 'images', 'tags']))
+    return res.json({ code: RESPONSE_CODE.OK, status: RESPONSE_STATUS.SUCCESS, data: highlight })
   })
 })
 
